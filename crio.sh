2697b22b1f4d09bac2f4eca14e53774a61db5ad2
Shourjo <54590698+shourjo-h@users.noreply.github.com>
2024-07-01 15:44:22 +0530

    Create crio.sh

---
+++ crio.sh
@@ -0,0 +1,4 @@
+file_path="/etc/crio/crio.conf"
+new_line="conmon = "/usr/bin/conmon""
+sed -i '121s/.*/'"$new_line"'/' "$file_path"
+echo "registries = ['docker.io', 'quay.io', 'registry.fedoraproject.org']" >> "$file_path"
