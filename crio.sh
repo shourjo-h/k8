bc7459bd2c31bc5e2dc97bd4e33700c741ce96ab
Shourjo <54590698+shourjo-h@users.noreply.github.com>
2024-07-01 23:26:49 +0530

    may-need-rool-back

--- crio.sh
+++ crio.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 file_path="/etc/crio/crio.conf"
 new_line="conmon = \"/usr/bin/conmon\""
+
+# Replace specific line in crio.conf
 sed -i "121s~.*~$new_line~" "$file_path"
-echo "[registries.search]" >> "$file_path"
-echo "registries = ['docker.io', 'quay.io', 'registry.fedoraproject.org']" >> "$file_path"
+
+# Append registries configuration to crio.conf
+cat <<EOF >> "$file_path"
+[registries.search]
+registries = ['docker.io', 'quay.io', 'registry.fedoraproject.org']
+EOF
+
+echo "Configuration updated in $file_path"
