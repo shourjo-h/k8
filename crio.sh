dc5a34538c537f68f90ed692e99964dd99b399a3
Shourjo <54590698+shourjo-h@users.noreply.github.com>
2024-07-01 17:20:27 +0530

    Update crio.sh

--- crio.sh
+++ crio.sh
@@ -1,5 +1,5 @@
 file_path="/etc/crio/crio.conf"
 new_line="conmon = \"/usr/bin/conmon\""
 sed -i "121s~.*~$new_line~" "$file_path"
+echo "[registries.search]" >> "$file_path"
 echo "registries = ['docker.io', 'quay.io', 'registry.fedoraproject.org']" >> "$file_path"
-
